02df39b1eaf21e0cb3c92d5b67fa88e100e0a234
dev-abhishekkr <dev.abhishekkr@gmail.com>
2024-12-29 18:11:41 +0530

    signin route

--- backend/routes/auth-route.js
+++ backend/routes/auth-route.js
@@ -5,6 +5,6 @@ import { signIn, signUp } from "./../controllers/auth-controller.js";
 const router = express.Router();
 
 router.post("/signup", signUp);
-router.post("/sigin", signIn);
+router.post("/signin", signIn);
 
 export default router;
