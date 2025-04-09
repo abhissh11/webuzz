d0ae3df473729621c6d6763e9ab70b03c9dcb4a6
dev-abhishekkr <dev.abhishekkr@gmail.com>
2025-04-09 13:30:38 +0530

    baseurl update

--- backend/index.js
+++ backend/index.js
@@ -17,13 +17,21 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "https://webuzz-chat.vercel.app",
+    ],
     methods: ["GET", "POST"],
     credentials: true,
   },
 });
 
-const allowedOrigin = ["http://localhost:3000", "http://localhost:5173"];
+const allowedOrigin = [
+  "http://localhost:3000",
+  "http://localhost:5173",
+  "https://webuzz-chat.vercel.app",
+];
 
 // Middlewares
 app.use(
